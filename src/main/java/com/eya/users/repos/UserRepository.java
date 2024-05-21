d6dbf1f6657fbc16d72e7b482bd90550b98559ed
eyamakhlouf <makhlouf.eya2022@gmail.com>
2024-05-21 21:14:14 +0100

    backend

---
+++ src/main/java/com/eya/users/repos/UserRepository.java
@@ -0,0 +1,9 @@
+package com.eya.users.repos;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.eya.users.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+		User findByUsername(String username);
+
+}
